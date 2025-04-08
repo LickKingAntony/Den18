68b9eb0db660cef2e51dd746ead0c9222e5e7946
CasualMan228 <casualman228@gmail.com>
2025-04-08 08:40:33 +0300

    MyFirstCommit

---
+++ WpfApp/Hotel.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collection.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CountOfStars { get; set; }
+        public string CountryName { get; set; }
+        public string HotelImage { get; set; }
+    }
+}
