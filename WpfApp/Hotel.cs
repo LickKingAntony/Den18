b4d33641c02aa7865444c843bce06d1b03441e48
LickKingAntony <puskintimofej802@gmail.com>
2025-04-10 12:50:01 +0300

    jfjdCommit

--- WpfApp/Hotel.cs
+++ WpfApp/Hotel.cs
@@ -13,5 +13,6 @@ namespace WpfApp
         public int CountOfStars { get; set; }
         public string CountryName { get; set; }
         public string HotelImage { get; set; }
+        //jfjd
     }
 }
